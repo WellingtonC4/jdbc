1359d71038d11a104a0e0de3794e1d9548e24c57
wellingtonc4 <wellingtontrack19@gmail.com>
2023-04-11 13:04:13 -0300

    Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -18,40 +19,33 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
 			
 			st = conn.prepareStatement(
-					"INSERT INTO seller"
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+ "VALUES"
-					+ "(?, ?, ?, ?, ?)");
 					
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.00);
-			st.setInt(5, 4);
+					"UPDATE seller "
+					+"SET BaseSalary = BaseSalary + ? "
+					+"WHERE "
+					+"(DepartmentId = ?)");
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
 			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done Rows affected: " + rowsAffected);
+			System.out.println("Done! Rows affected: " + rowsAffected);
+			
 		}
-
-		catch (SQLException e ) {
+		catch (SQLException e) {
 			e.printStackTrace();
 		}
-		catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
-		finally{
+		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
+		
 		
 
 	}
